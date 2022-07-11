99da0cc3e4410c369082f7650a6b91fafb7cfdd0
monikasananse <monikasnns@gmail.com>
2022-07-11 19:33:06 +0530

    average

---
+++ Average.js
@@ -0,0 +1,20 @@
+// You are given anarrwhich hasNintegers.
+
+// Write a program to print the ceil ofaverage of integerspresent in the array.
+
+// Note:- The ceil value of the given whole numberKis the smallest integer value greater or equal to aK.
+
+// For example:-
+
+// if the value of K = 3.679, the ceil value of K is 4
+// if the value of K = 5, the ceil value of K is 5.
+function averageOfAll(n, arr){
+    //write code here
+    var sum=0;
+    for(var i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    console.log(Math.ceil(sum/n));
+}
+averageOfAll(5,[1,2,1,3,4])
