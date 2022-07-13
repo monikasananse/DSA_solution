519d3b3c0aa437d564e88077ad3b828e3482ab66
monikasananse <monikasnns@gmail.com>
2022-07-13 14:06:29 +0530

    Minimum

---
+++ Maximum_array.js
@@ -0,0 +1,32 @@
+// You are given an array, stored in a variable with the sizearr
+
+// The size of the array is stored in a variable with the namen
+
+// You have to find the maximum number in the array
+
+// For example, consider the array asarr = [1 2 3 4 5], and the value stored in n = 5
+
+// Then, the required output will be5, as it is the largest number in the array
+
+// Sample Input 1 
+
+// 5
+// 1 2 3 4 5
+
+// Sample Output 1
+
+// 5
+
+function maximumInArray(N,arr){
+    //write code here
+var max=-Infinity;
+for(var i=0;i<N;i++)
+{
+    if(max<arr[i])
+    {
+        max=arr[i];
+    }
+}
+console.log(max);
+}
+maximumInArray(5,[89,6,45,999,23])
