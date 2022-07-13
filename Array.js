e794a789906774c110589d7aeefa103160b0409d
monikasananse <monikasnns@gmail.com>
2022-07-13 13:21:14 +0530

    Array

---
+++ Array.js
@@ -0,0 +1,25 @@
+// You are given an array, whose size is stored in a variable of sizen
+
+// The array is stored in a variable with the name,arr
+
+// You have to traverse the array, and print each element on a new line
+
+// For example, the value stored inN = 5, and the array isarr = [1 2 3 4 5]
+
+// Therefore, the required output will be 
+
+// 1
+// 2
+// 3
+// 4
+// 5
+
+function printArray(N,arr){
+    //write code here
+for(var i=0;i<N;i++)
+{
+    console.log(arr[i]);
+}
+}
+
+printArray(5,[11,12,13,14,15])
