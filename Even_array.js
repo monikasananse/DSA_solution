f8e7ef5235f819f5ebf138d4032456bda6ca6bbc
monikasananse <monikasnns@gmail.com>
2022-07-13 14:27:29 +0530

    Even_elements

---
+++ Even_array.js
@@ -0,0 +1,30 @@
+// You are given an array, stored in a variable with the sizearr
+
+// The size of the array is stored in a variable with the namen
+
+// You have to traverse the array, and print only the even elements in the array.
+
+// Print each number on a new line
+
+// For example, consider the array asarr = [1 2 3 4 5], and the value stored inn = 5
+
+// Then, the required output will be
+
+// 2
+// 4
+
+function evenArray(n, arr){
+    //write code here
+for(var i=0;i<n;i++)
+{
+    if(arr[i]%2==0)
+    {
+        console.log(arr[i]);
+    }
+    else
+    {
+        continue;
+    }
+}
+}
+evenArray(5,[11,0,6,13,9])
