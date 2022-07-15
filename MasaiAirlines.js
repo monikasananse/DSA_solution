9154aa4a66c8ac42380fda946ccea76c54f17ae3
monikasananse <monikasnns@gmail.com>
2022-07-15 19:17:27 +0530

    MasaiAirlines

---
+++ MasaiAirlines.js
@@ -0,0 +1,39 @@
+// You are working for Masai Airline. There are **n** passengers with 2 bags each (one check-in and other hand bag). The limitation for check-in bag is 15kgs and that for hand bag is 7kgs. You are given 2 arrays: First array contains weight of check-in bag of ith passenger. Similarly, second array contains weights of hand bag of ith passenger. Print "Boarding" (without quotes) if the passenger clears both luggage weight limits of Masai Airline, else print "Stop" (without quotes).
+
+// Please note that 15 kgs and 7 kgs are inclusive.
+
+// Note : You have to take the input yourself
+
+// Constraints :
+
+// n<1000
+
+// Output "Boarding" (without quotes) or "Stop" (without quotes) of the passenger meets criteria of Masai Airlines.
+
+// Sample Input 1 
+
+// 4
+// 12 14 15 6
+// 8 5 7 4
+// Sample Output 1
+
+// Stop
+// Boarding
+// Boarding
+// Boarding
+
+//Enter code here
+function logic(n,checkin,handbag){    
+    for(var i=0;i<n;i++)
+    {
+        if(checkin[i]<=15 && handbag[i]<=7)
+        {
+            console.log("Boarding");
+        }
+        else
+        {
+            console.log("Stop");
+        }
+    }
+}
+logic(4,[12,14,15,6],[8,5,7,4])
