368b96a340e60115f0ade0bf8236bfb78acac0f2
monikasananse <monikasnns@gmail.com>
2022-07-23 15:28:25 +0530

    ques

---
+++ Difference_of_k.js
@@ -0,0 +1,36 @@
+// Description
+
+// You are given a sorted arrayAof sizeN, and another integerK
+// You have to find if there exists a pair of integers with indexiandj, such thati != j, andA[i] - A[j] = k
+// If such a pair exists, printYes, else printNo
+
+// Input
+// The first line of the input containsT, the number of test cases.
+
+// The first line of each test case contains N and K, the size of the array and the value of K.
+
+// The next line contains N space separated integers denoting the elements of the array
+
+// Constraints
+
+// 1 <=T<= 10
+
+// 1 <=N<= 1000000
+
+// -10^9 <=A[i]<= 10^9
+
+// 0 <= K <= 10^9
+
+// Sample Input 1 
+
+// 2
+// 5 3
+// 1 2 3 4 5
+// 5 8
+// 1 2 3 4 5 
+
+// Sample Output 1
+
+// Yes
+// No
+
