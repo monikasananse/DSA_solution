9884b210afeb39babcf9a9d1fef7f17385b34b12
monikasananse <monikasnns@gmail.com>
2022-08-07 19:29:14 +0530

    done

---
+++ javascript-5/Problem4.js
@@ -0,0 +1,21 @@
+// Given 3 numbers (all different values), print which is greatest;
+
+var a=10;
+var b=15;
+var c=10;
+if(a>b && a>c)
+{
+    console.log("a is greatest")
+}
+else if(b>a && b>c)
+{
+    console.log("b is greatest");
+}
+else if(c>a && c>b)
+{
+    console.log("c is greatest");
+}
+else
+{
+    console.log("all are equal");
+}
