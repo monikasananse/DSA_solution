12f896688defe7b968ff2280b526092ede276877
monikasananse <monikasnns@gmail.com>
2022-08-07 19:23:28 +0530

    done

---
+++ javascript-5/Problem2.js
@@ -0,0 +1,11 @@
+// Given any character, if it is a vowel print "Vowel"
+
+var char="m";
+if(char==="a"||char==="e"||char==="i"||char==="o"||char==="u")
+{
+    console.log("vowel")
+}
+else
+{
+    console.log("consonant")
+}
