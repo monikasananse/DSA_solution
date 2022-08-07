06922add3d7862dd66816e4de7cc33f2349145f4
monikasananse <monikasnns@gmail.com>
2022-08-07 19:25:30 +0530

    done

---
+++ javascript-5/Problem3.js
@@ -0,0 +1,10 @@
+// Given and character if it is a consonant print "Consonant";
+
+var char="p";
+if(char!="a"||char!=="e"||char!=="i"||char!=="o"||char!=="u")
+{
+    console.log("consonant");
+}
+else{
+    console.log("vowel")
+}
