12c694db5754ed43620ad1f091dcb518380dd392
monikasananse <monikasnns@gmail.com>
2022-08-08 11:22:13 +0530

    done

---
+++ javascript-LogicalOperator/CompareTwoSum.js
@@ -0,0 +1,55 @@
+// Description
+
+// You are given six numbers, stored in the variables with the following names
+
+// one, two, three, four, five, six
+
+// Find the value ofsum1andsum2, such that
+
+// sum1 = one + two
+
+// sum2 = four + five
+
+// If both the following two conditions are true, printYes, else printNo
+
+// sum1 > three
+
+// sum2 > six
+
+// Input
+// First and the only line of input contains, the values stored inone, two, three, four, five, six
+
+// Output
+// PrintYes, if both the conditions given in the problem statement are true, else printNo
+
+// Sample Input 1 
+// 1 2 3 4 5 6
+// Sample Output 1
+// No
+
+// Hint
+// In the sample test case, the value stored inone = 1, two = 2, three = 3, four = 4, five = 5, six = 6
+// Therefore, the value of sum1 and sum2 becomes
+
+// sum1 = one + two = 1 + 2 = 3
+
+// sum2 = four + five = 4 + 5 = 9
+
+// Now, the value of sum2 is greater than the value stored in six, but the value stored in sum1 is not greater than the value stored in three, Therefore, the answer is No
+
+
+function twoSumsOneNumber(one, two, three, four, five, six) {
+    // Write code here
+    var sum1=one+two;
+    var sum2=four+five;
+    
+    if(sum1>three && sum2>six)
+    {
+        console.log("Yes");
+    }
+    else
+    {
+        console.log("No");
+    }
+}
+twoSumsOneNumber(46,9,23,79,25,79);
