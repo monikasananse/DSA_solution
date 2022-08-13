786d0cab4afccc52a82c78076e0d7bc2c678866f
monikasananse <monikasnns@gmail.com>
2022-08-13 11:11:38 +0530

    done

---
+++ javascript__6_whileLoop/Print_Monika.js
@@ -0,0 +1,33 @@
+// You are given a number stored in a variable with the name num
+
+// You have to printMasai School, as many times as the number stored in the variable num
+
+// Every time, printMasai Schoolon a new line
+
+// Input
+// The first and the only line of input contains a number stored in the variable num
+// Output
+// PrintMasai School, as many times as the number stored in the variable num, every time on a new line
+
+// Sample Input 1 
+// 5
+// Sample Output 1
+// Masai School
+// Masai School
+// Masai School
+// Masai School
+// Masai School
+
+//using while loop
+
+function print(num)
+{
+    var i=1;
+    while(i<=num)
+    {
+        console.log("Masai School")
+        i++;
+    }
+}
+print(5)
+
