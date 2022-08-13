ee0f6c5537999e17dd0c4134ece5c391fbdc3a81
monikasananse <monikasnns@gmail.com>
2022-08-13 12:24:55 +0530

    done

---
+++ javascript__6_whileLoop/Even_sum_Below_N.js
@@ -0,0 +1,44 @@
+// Description
+
+// You are given a number, stored in a variable with the namenum
+
+// Find the sum of all even numbers greater than zero, and less than or equal to the value stored innum
+
+// For example, if the value stored innum = 5, then all the even numbers smaller than or equal to 5, and greater than zero are
+
+// 2
+// 4
+// Therefore, the sum becomes,sum = 2 + 4 = 6, which is the required output
+
+
+// Input
+// The first and the only line of input contains the number stored in the variablenum
+
+
+// Output
+// Print the sum of all even numbers greater than zero, and less than or equal to the value stored innum
+
+
+// Sample Input 1 
+
+// 6
+// Sample Output 1
+
+// 12
+
+function evenSumBelowN(num) {
+    // Write code here
+var i=1;
+var sum=0;
+while(i<=num)
+{
+    if(i%2==0)
+    {
+        sum=sum+i;
+    }
+    i++;
+}
+console.log(sum);
+  
+}
+evenSumBelowN(13);
